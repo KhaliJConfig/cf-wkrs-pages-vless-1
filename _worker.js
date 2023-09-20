5dc34eb70765f496a1a6860284a249719cbc8bb7
0xHO <0xHO>
2023-09-20 11:40:35 +0800

    update Wed Sep 20 11:40:35 CST 2023,

--- _worker.js
+++ _worker.js
@@ -6,17 +6,10 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'f20c6fcd-2893-45bb-985e-5667ba50c9de';
 
-const proxyIPs = ['cdn-all.xn--b6gac.eu.org', 'cdn.xn--b6gac.eu.org', 'cdn-b100.xn--b6gac.eu.org', 'edgetunnel.anycast.eu.org', 'cdn.anycast.eu.org'];
+const proxyIPs = ['us-1.xstream2.com', 'us-2.xstream2.com', 'us-3.xstream2.com'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
 
 let dohURL = 'https://1.1.1.1/dns-query'; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query or https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg=
-
-// v2board api environment variables
-let nodeId = ''; // 1
-
-let apiToken = ''; //abcdefghijklmnopqrstuvwxyz123456
-
-let apiHost = ''; // api.v2board.com
 
 if (!isValidUUID(userID)) {
     throw new Error("uuid is not valid ${ userID }");
@@ -34,9 +27,6 @@ export default {
             userID = env.UUID || userID;
             proxyIP = env.PROXYIP || proxyIP;
             dohURL = env.DNS_RESOLVER_URL || dohURL;
-            nodeId = env.NODE_ID || nodeId;
-            apiToken = env.API_TOKEN || apiToken;
-            apiHost = env.API_HOST || apiHost;
             const upgradeHeader = request.headers.get('Upgrade');
             if (!upgradeHeader || upgradeHeader !== 'websocket') {
                 const url = new URL(request.url);
@@ -90,8 +80,8 @@ export default {
                             return new Response(connectError.message, { status: 500 });
                         }
                     case `/${userID}`: {
-                        const vlessConfig = getVLESSConfig(userID, request.headers.get('Host'));
-                        return new Response(`${vlessConfig}`, {
+                        const hostName = request.headers.get('Host');
+                        return new Response(`success:${userID}@${hostName}`, {
                             status: 200,
                             headers: {
                                 "Content-Type": "text/plain;charset=utf-8",
@@ -219,81 +209,6 @@ async function vlessOverWSHandler(request) {
     });
 }
 
-let apiResponseCache = null;
-let cacheTimeout = null;
-
-/**
- * Fetches the API response from the server and caches it for future use.
- * @returns {Promise<object|null>} A Promise that resolves to the API response object or null if there was an error.
- */
-async function fetchApiResponse() {
-    const requestOptions = {
-        method: 'GET',
-        redirect: 'follow'
-    };
-
-    try {
-        const response = await fetch(`https://${apiHost}/api/v1/server/UniProxy/user?node_id=${nodeId}&node_type=v2ray&token=${apiToken}`, requestOptions);
-
-        if (!response.ok) {
-            console.error('Error: Network response was not ok');
-            return null;
-        }
-        const apiResponse = await response.json();
-        apiResponseCache = apiResponse;
-
-        // Refresh the cache every 5 minutes (300000 milliseconds)
-        if (cacheTimeout) {
-            clearTimeout(cacheTimeout);
-        }
-        cacheTimeout = setTimeout(() => fetchApiResponse(), 300000);
-
-        return apiResponse;
-    } catch (error) {
-        console.error('Error:', error);
-        return null;
-    }
-}
-
-/**
- * Returns the cached API response if it exists, otherwise fetches the API response from the server and caches it for future use.
- * @returns {Promise<object|null>} A Promise that resolves to the cached API response object or the fetched API response object, or null if there was an error.
- */
-async function getApiResponse() {
-    if (!apiResponseCache) {
-        return await fetchApiResponse();
-    }
-    return apiResponseCache;
-}
-
-/**
- * Checks if a given UUID is present in the API response.
- * @param {string} targetUuid The UUID to search for.
- * @returns {Promise<boolean>} A Promise that resolves to true if the UUID is present in the API response, false otherwise.
- */
-async function checkUuidInApiResponse(targetUuid) {
-    // Check if any of the environment variables are empty
-    if (!nodeId || !apiToken || !apiHost) {
-        return false;
-    }
-
-    try {
-        const apiResponse = await getApiResponse();
-        if (!apiResponse) {
-            return false;
-        }
-        const isUuidInResponse = apiResponse.users.some(user => user.uuid === targetUuid);
-        return isUuidInResponse;
-    } catch (error) {
-        console.error('Error:', error);
-        return false;
-    }
-}
-
-// Usage example:
-//   const targetUuid = "65590e04-a94c-4c59-a1f2-571bce925aad";
-//   checkUuidInApiResponse(targetUuid).then(result => console.log(result));
-
 /**
  * Handles outbound TCP connections.
  *
@@ -415,10 +330,7 @@ function makeReadableWebSocketStream(webSocketServer, earlyDataHeader, log) {
  * @param {string} userID 
  * @returns 
  */
-async function processVlessHeader(
-    vlessBuffer,
-    userID
-) {
+async function processVlessHeader(vlessBuffer, userID) {
     if (vlessBuffer.byteLength < 24) {
         return {
             hasError: true,
@@ -426,24 +338,11 @@ async function processVlessHeader(
         };
     }
     const version = new Uint8Array(vlessBuffer.slice(0, 1));
-    let isValidUser = false;
     let isUDP = false;
     const slicedBuffer = new Uint8Array(vlessBuffer.slice(1, 17));
     const slicedBufferString = stringify(slicedBuffer);
 
     const uuids = userID.includes(',') ? userID.split(",") : [userID];
-
-    const checkUuidInApi = await checkUuidInApiResponse(slicedBufferString);
-    isValidUser = uuids.some(userUuid => checkUuidInApi || slicedBufferString === userUuid.trim());
-
-    console.log(`checkUuidInApi: ${await checkUuidInApiResponse(slicedBufferString)}, userID: ${slicedBufferString}`);
-
-    if (!isValidUser) {
-        return {
-            hasError: true,
-            message: 'invalid user',
-        };
-    }
 
     const optLength = new Uint8Array(vlessBuffer.slice(17, 18))[0];
     //skip opt for now
@@ -737,44 +636,4 @@ async function handleUDPOutBound(webSocket, vlessResponseHeader, log) {
             writer.write(chunk);
         }
     };
-}
-
-/**
- * 
- * @param {string} userID 
- * @param {string | null} hostName
- * @returns {string}
- */
-function getVLESSConfig(userID, hostName) {
-    const vlessLink = `vless://${userID}@${hostName}:80?encryption=none&security=none&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}`
-    const vlessTlsLink = `vless://${userID}@${hostName}:443?encryption=none&security=tls&sni=${hostName}&fp=randomized&type=ws&host=${hostName}&path=%2F%3Fed%3D2048#${hostName}`
-    return `
-下面是非 TLS 端口的节点信息及分享链接，可使用 CF 支持的非 TLS 端口：
-
-地址：${hostName} 或 CF 优选 IP
-端口：80 或 CF 支持的非 TLS 端口
-UUID：${userID}
-传输：ws
-伪装域名：${hostName}
-路径：/?ed=2048
-
-${vlessLink}
-
-下面是 TLS 端口的节点信息及分享链接，可使用 CF 支持的 TLS 端口：
-
-地址：${hostName} 或 CF 优选 IP
-端口：443 或 CF 支持的 TLS 端口
-UUID：${userID}
-传输：ws
-传输层安全：TLS
-伪装域名：${hostName}
-路径：/?ed=2048
-SNI 域名：${hostName}
-
-${vlessTlsLink}
-
-提示：如使用 workers.dev 域名，则无法使用 TLS 端口
----------------------------------------------------------------
-更多教程，请关注：小御坂的破站
-`;
 }
